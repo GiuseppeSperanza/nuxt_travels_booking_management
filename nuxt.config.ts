fd0f63a93808326e0e812b7473d47068b2fe00ff
Giuseppe Speranza <giuseppesperanza1996@gmail.com>
2024-03-02 13:06:55 +0100

    chore: added project scaffold

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: process.env.NUXT_PAGE_TITLE,
+    }
+  },
   devtools: { enabled: true }
 })
